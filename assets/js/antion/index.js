0194e23125fda9140721399ed764da12aaa9b47e
alu <alu@aludeiMac.local>
2016-07-05 14:30:22 +0800

    update

--- assets/js/antion/index.js
+++ assets/js/antion/index.js
@@ -1,24 +1,37 @@
 (function($){
 
-    
-    
-    $(".logo").on("mouseover",function(){
-        
-        
-        $(this).unbind("mouseover");
-        
-        var timer =setTimeout(function(){
-            $(".logo_anit").removeClass("logo_anit");
-            $(".logo").animate({top:-450},1000,function(){
+
+
+
+
+
+        $(".logo").on("mouseover",function(){
+
+            if($('body').width()<720){
+                clearTimeout(timer);
                 
-                $(".content").animate({opacity:1},500);
-            
-            });
-           
-            clearTimeout(timer);
-        },1000);
-        
-    }); 
+            }else {
+                $(this).unbind("mouseover");
+
+                var timer =setTimeout(function(){
+                    $(".logo_anit").removeClass("logo_anit");
+                    $(".logo").animate({top:-450},1000,function(){
+
+                        $(".content").animate({opacity:1},500);
+
+                    });
+
+                    clearTimeout(timer);
+                },1000);
+
+            }
+
+
+
+        });
+
+
+
     
 
 })(jQuery);
